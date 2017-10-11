7430af47903478be6c3ed39c9a31bbbeb1e14018
kenu <kenu.heo@gmail.com>
2017-10-12 08:20:21 +0900

    database schema

---
+++ photogallery/docs/photogallery.sql
@@ -0,0 +1,34 @@
+GRANT ALL PRIVILEGES ON devcamp.* TO javauser@localhost
+ IDENTIFIED BY 'javadude' WITH GRANT OPTION;
+drop database devcamp;
+create database devcamp;
+
+create table devcamp.photogallery (
+  id int not null auto_increment primary key,
+  writer varchar(50),
+  title varchar(100),
+  content text,
+  filename VARCHAR (255),
+  savename VARCHAR (20),
+  filesize int);
+
+insert into devcamp.photogallery (writer, title, content) values ('kenu', 'kenu.heo@gmail.com', 'content');
+
+
+GRANT ALL PRIVILEGES ON javatest.* TO javauser@localhost
+ IDENTIFIED BY 'javadude' WITH GRANT OPTION;
+
+drop database javatest;
+
+create database javatest;
+
+create table javatest.photogallery (
+  id int not null auto_increment primary key,
+  writer varchar(50),
+  title varchar(100),
+  content text,
+  filename VARCHAR (255),
+  savename VARCHAR (20),
+  filesize int);
+
+insert into javatest.photogallery (writer, title, content) values ('kenu', 'kenu.heo@gmail.com', 'content');
