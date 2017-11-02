f5c825652d3006903a57360b7f1b0d5f889b3188
kenu <kenu.heo@gmail.com>
2017-11-02 15:47:17 +0900

    rename

--- spring/spring-boot-jsp/deploy.sh
+++ spring/spring-boot-jsp/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo service myapp stop
-sleep 10;
-cp -f ./target/hello-springboot-1.5.8.RELEASE.war /home/ec2-user/hello-springboot-1.5.8.RELEASE.war
-chmod 755 /home/ec2-user/hello-springboot-1.5.8.RELEASE.war
+sleep 5;
+cp -f ./target/hello-springboot-1.5.8.RELEASE.war /home/ec2-user/spring-boot-jsp.jar
+chmod 755 /home/ec2-user/spring-boot-jsp.jar
 sudo rm -rf /etc/init.d/myapp
-sudo ln -s /home/ec2-user/hello-springboot-1.5.8.RELEASE.war /etc/init.d/myapp
+sudo ln -s /home/ec2-user/spring-boot-jsp.jar /etc/init.d/myapp
 sudo service myapp start
