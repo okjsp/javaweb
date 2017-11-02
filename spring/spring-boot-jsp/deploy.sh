4542c50e2e6f8063701eda9c92f8c7204c0ec7dc
kenu <kenu.heo@gmail.com>
2017-11-02 15:56:07 +0900

    deploy

--- spring/spring-boot-jsp/deploy.sh
+++ spring/spring-boot-jsp/deploy.sh
@@ -2,8 +2,8 @@
 sudo service myapp stop
 sleep 5;
 echo `pwd`
-cp -f spring/spring-boot-jsp/target/hello-springboot-1.5.8.RELEASE.war /home/ec2-user/spring-boot-jsp.jar
-chmod 755 /home/ec2-user/spring-boot-jsp.jar
+cp -f spring/spring-boot-jsp/target/hello-springboot-1.5.8.RELEASE.war /home/ec2-user/hello-springboot-1.5.8.RELEASE.war
+chmod 755 /home/ec2-user/hello-springboot-1.5.8.RELEASE.war
 sudo rm -rf /etc/init.d/myapp
-sudo ln -s /home/ec2-user/spring-boot-jsp.jar /etc/init.d/myapp
+sudo ln -s /home/ec2-user/hello-springboot-1.5.8.RELEASE.war /etc/init.d/myapp
 sudo service myapp start
